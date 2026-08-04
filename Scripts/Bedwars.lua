46aa1f7b2721e7b7dfbdae4e457ba33bbb36fc34
nrwkrieger <l.engelbrecht2701@gmail.com>
2026-08-04 10:21:16 +0000

    Fix slider implementation for Distance setting

--- Scripts/Bedwars.lua
+++ Scripts/Bedwars.lua
@@ -1,3 +1,4 @@
+
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/nrwkrieger/markusS-derHurensohn/main/Libary/Lurk.lua"))() or LurkUI
 local HttpService = game:GetService("HttpService")
 
